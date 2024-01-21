4105ee01e9a16ec69d4e8cf59da3c60096a6e739
samson <samsonaderonmu2021@gmail.com>
2024-01-21 15:05:38 +0100

    ui development

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,9 +1,27 @@
+"use client";
 
+import { axios } from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
-export default Register
+  const onSignUp = async () => {};
+  const router = useRouter();
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Sign Up</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input type="text" id="username" value={user.username} onChange={(e)=>setUser({...user, username:e.target.value})} />
+    </div>
+  );
+};
+
+export default Register;
